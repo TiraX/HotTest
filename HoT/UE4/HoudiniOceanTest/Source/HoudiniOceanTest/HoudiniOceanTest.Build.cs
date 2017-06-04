52e9bf902adbc3fcbeca55150fb2a3090df6cd97
tirax <99733085@qq.com>
2017-06-04 10:43:28 +0800

    modified build.cs add the include and library path.

--- HoT/UE4/HoudiniOceanTest/Source/HoudiniOceanTest/HoudiniOceanTest.Build.cs
+++ HoT/UE4/HoudiniOceanTest/Source/HoudiniOceanTest/HoudiniOceanTest.Build.cs
@@ -4,11 +4,46 @@ using UnrealBuildTool;
 
 public class HoudiniOceanTest : ModuleRules
 {
+    //private string ModulePath
+    //{
+    //    get { return Path.GetDirectoryName(RuleCompiler.GetModuleFilename(this.GetType().Name)); }
+    //}
+
+    private string ThirdPartyPath
+    {
+        get { return "D:/Unreal Projects/HotTest/HoT/UE4/HoudiniOceanTest/3rdparty/"; }
+    }
+
+    public bool LoadLibs(TargetInfo Target)
+    {
+        bool isLibrarySupported = false;
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibrarySupported = true;
+
+            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "win64" : "win32";
+            string LibrariesPath = ThirdPartyPath + PlatformString + "/";
+
+            PublicAdditionalLibraries.Add(LibrariesPath + "libfftw3-3.lib");
+            PublicAdditionalLibraries.Add(LibrariesPath + "libfftw3f-3.lib");
+            PublicAdditionalLibraries.Add(LibrariesPath + "libfftw3l-3.lib");
+
+            PublicIncludePaths.Add(LibrariesPath);
+            PublicIncludePaths.Add(ThirdPartyPath + "include/");
+        }
+
+        //Definitions.Add(string.Format("WITH_FFTW_LIB_BINDING={0}", isLibrarySupported ? 1 : 0));
+
+        return isLibrarySupported;
+    }
+
 	public HoudiniOceanTest(TargetInfo Target)
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI", "RenderCore", "ShaderCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        LoadLibs(Target);
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
--- HoT/UE4/HoudiniOceanTest/Source/HoudiniOceanTest/OceanMeshComponent.cpp
+++ HoT/UE4/HoudiniOceanTest/Source/HoudiniOceanTest/OceanMeshComponent.cpp
@@ -309,9 +309,9 @@ private:
 };
 
 //////////////////////////////////////////////////////////////////////////
-#pragma comment (lib, "libfftw3-3.lib")
-#pragma comment (lib, "libfftw3f-3.lib")
-#pragma comment (lib, "libfftw3l-3.lib")
+//#pragma comment (lib, "libfftw3-3.lib")
+//#pragma comment (lib, "libfftw3f-3.lib")
+//#pragma comment (lib, "libfftw3l-3.lib")
 
 
 UOceanMeshComponent::UOceanMeshComponent(const FObjectInitializer& ObjectInitializer)
